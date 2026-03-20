ec984a381e5b46d2241bee621f04c5322719299d
Emberwind-Studios <89319126+Emberwind-Studios@users.noreply.github.com>
2026-03-20 08:50:38 -0400

    Initial project setup

---
+++ app/src/App.tsx
@@ -0,0 +1,93 @@
+import Header from "../components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <section className="flex justify-center items-center w-[100%] h-100 ">
+        <div className="grid grid-cols-2 gap-4 grow-1 justify-items-center">
+          <div>
+            <div className="card card-border bg-base-100 w-96 shadow-lg">
+              <div className="card-body">
+                <input type="text" placeholder="Name" className="input" />
+                <label className="input validator">
+                  <svg
+                    className="h-[1em] opacity-50"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <g
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
+                      strokeWidth="2.5"
+                      fill="none"
+                      stroke="currentColor"
+                    >
+                      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                    </g>
+                  </svg>
+                  <input type="email" placeholder="mail@site.com" required />
+                </label>
+                <div className="validator-hint hidden">
+                  Enter valid email address
+                </div>
+                <label className="input validator">
+                  <svg
+                    className="h-[1em] opacity-50"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 16 16"
+                  >
+                    <g fill="none">
+                      <path
+                        d="M7.25 11.5C6.83579 11.5 6.5 11.8358 6.5 12.25C6.5 12.6642 6.83579 13 7.25 13H8.75C9.16421 13 9.5 12.6642 9.5 12.25C9.5 11.8358 9.16421 11.5 8.75 11.5H7.25Z"
+                        fill="currentColor"
+                      ></path>
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M6 1C4.61929 1 3.5 2.11929 3.5 3.5V12.5C3.5 13.8807 4.61929 15 6 15H10C11.3807 15 12.5 13.8807 12.5 12.5V3.5C12.5 2.11929 11.3807 1 10 1H6ZM10 2.5H9.5V3C9.5 3.27614 9.27614 3.5 9 3.5H7C6.72386 3.5 6.5 3.27614 6.5 3V2.5H6C5.44771 2.5 5 2.94772 5 3.5V12.5C5 13.0523 5.44772 13.5 6 13.5H10C10.5523 13.5 11 13.0523 11 12.5V3.5C11 2.94772 10.5523 2.5 10 2.5Z"
+                        fill="currentColor"
+                      ></path>
+                    </g>
+                  </svg>
+                  <input
+                    type="tel"
+                    className="tabular-nums"
+                    required
+                    placeholder="Phone"
+                    pattern="[0-9]*"
+                    minLength="10"
+                    maxLength="10"
+                    title="Must be 10 digits"
+                  />
+                </label>
+                <p className="validator-hint">Must be 10 digits</p>
+
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Submit</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="card card-border bg-base-100 w-96">
+              <div className="card-body">
+                <h2 className="card-title">Card Title</h2>
+                <p>
+                  A card component has a figure, a body part, and inside body
+                  there are title and actions parts
+                </p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
